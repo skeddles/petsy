fbb3fe03919314707593987a702ec167d41fa5c0
Sam Keddy <skeddy13@yahoo.com>
2024-10-11 16:58:04 -0400

    removed old crap

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 import { Blurb } from "./ui/blurb";
 
 export default function Home() {
-  return (
-	<Blurb />
-  );
+	return (
+		<Blurb />
+	);
 }
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,20 +4,7 @@ import { useState } from 'react';
 import BuyButton from '../../app/buy-button.tsx';
 
 export default function Product() {
-	const router = useRouter()
-	const {id} = router.query
-	let [count, incCount] = useState(0);
-
-	function clickedBuy() {
-		incCount(count+1);
-		console.log(count);
-	}
-
-	return (<>
-		<Head>
-			<title>{id + '- Petsy'}</title>
-		</Head>
-		<h1>Product: {id}</h1>
-		<BuyButton />
-	</>)
+	return (
+		<h1>Product</h1>
+	)
 }
