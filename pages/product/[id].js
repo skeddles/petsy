593161597c207245980c4c9a805d3aa64de21299
Sam Keddy <skeddy13@yahoo.com>
2024-10-11 17:03:00 -0400

    fixed product page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,7 +1,4 @@
-import { useRouter } from 'next/router'
-import Head from 'next/head'
-import { useState } from 'react';
-import BuyButton from '../../app/buy-button.tsx';
+
 
 export default function Product() {
 	return (
